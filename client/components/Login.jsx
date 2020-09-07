66cdfd442627ff86a643b0c11a6f7b223a9a246c
Jake-Hurley <jshurley97@gmail.com>
2020-09-07 12:02:58 +1200

    adds error to login page on failed login

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -10,7 +10,8 @@ export class Login extends Component {
     state = {
         userName: '',
         password: '',
-        userData: {}
+        userData: {},
+        loginCheck: true
         // userData: { userId: 1, name: 'Jake', password: 'hurley'} 
     }
 
@@ -40,12 +41,14 @@ export class Login extends Component {
                        })
                 } else {
                     console.log('Username or password is incorrect')
+                    this.setState({loginCheck: false})
                 }
             }
         })
     }
 
     render () {
+        if(this.state.loginCheck === true){
         return (
             <div className='login-container'>
                 {/* ADD LOGO OF GAMESNIGHT */}
@@ -57,6 +60,20 @@ export class Login extends Component {
                 <Link className='register' to='/user/register'>Register</Link>
             </div>
         )
+    } else {
+        return (
+            <div className='login-container'>
+                {/* ADD LOGO OF GAMESNIGHT */}
+                <h3 className='login-title'>Login</h3>
+                <input className='user-input' onChange={this.changeHandler} name='userName' placeholder='userName'></input>
+                <input className='user-input' type='password' onChange={this.changeHandler} name='password' placeholder='password'></input>
+                <p className='login-error'>UserName or Password is incorrect</p>
+                <button className='login-button' onClick={() => this.clickHandler()}>Login</button>
+                <h5 className='new-here'>New here?</h5>
+                <Link className='register' to='/user/register'>Register</Link>
+            </div>
+            )
+        }
     }
 }
 
