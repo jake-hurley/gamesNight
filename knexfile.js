3d5e279018675b71587437e6ebac15adebfcb033
Jake-Hurley <jshurley97@gmail.com>
2020-09-09 21:12:11 +1200

    reconfig knex.js

--- knexfile.js
+++ knexfile.js
@@ -1,3 +1,52 @@
+// const path = require('path')
+
+// module.exports = {
+
+//   development: {
+//     client: 'sqlite3',
+//     connection: {
+//       filename: path.join(__dirname, 'dev.sqlite3')
+//     }
+//   },
+
+//   staging: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password',
+//       url: process.env.DATABASE_URL
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations'
+//     }
+//   },
+
+//   production: {
+//     client: 'postgresql',
+//     connection: {
+//       database: 'my_db',
+//       user:     'username',
+//       password: 'password',
+//       url: process.env.DATABASE_URL
+//     },
+//     pool: {
+//       min: 2,
+//       max: 10
+//     },
+//     migrations: {
+//       tableName: 'knex_migrations',
+//       directory: path.join(__dirname, 'migrations')
+//     }
+//   }
+
+// };
+
+
 const path = require('path')
 
 module.exports = {
@@ -6,35 +55,27 @@ module.exports = {
     client: 'sqlite3',
     connection: {
       filename: path.join(__dirname, 'dev.sqlite3')
-    }
+    },
+    useNullAsDefault: true
   },
 
-  staging: {
-    client: 'postgresql',
+  test: {
+    client: 'sqlite3',
     connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password',
-      url: process.env.DATABASE_URL
+      filename: ':memory:'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
+    useNullAsDefault: true,
     migrations: {
-      tableName: 'knex_migrations'
+      directory: path.join(__dirname, 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'seeds')
     }
   },
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password',
-      port: 36730,
-      url: process.env.DATABASE_URL
-    },
+    connection: process.env.DATABASE_URL,
     pool: {
       min: 2,
       max: 10
@@ -45,4 +86,4 @@ module.exports = {
     }
   }
 
-};
+}
